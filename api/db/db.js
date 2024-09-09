86b32a0a5a0ec6f4c1c9ae77afcc3c9e140433d6
ajhoobi <salahudeenibnnaser@gmail.com>
2024-09-09 09:32:53 +0530

    second commit

--- api/db/db.js
+++ api/db/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'db',      // Use the 'db' service name for the MySQL container
+  host: process.env.DB_HOST || 'mysql',      // Use the 'db' service name for the MySQL container
   user: process.env.DB_USER || 'root',    // Use the DB_USER from .env or default to 'root'
   password: process.env.DB_PASSWORD || 'salah123', // Use the DB_PASSWORD from .env or default to 'salah123'
   database: process.env.DB_NAME || 'web_data'  // Use the DB_NAME from .env or default to 'web_data'
